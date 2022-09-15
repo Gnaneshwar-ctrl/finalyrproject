16339815c1fed53f4575148ba7686135b87331ca
Gnaneshwar <gnaneshwarsampathirao13@gmail.com>
2022-09-15 23:53:46 +0530

    first commit

--- lampsinput.sh
+++ lampsinput.sh
@@ -2,9 +2,6 @@
 
 atomsk --create fcc 4.046 Al orient [100] [010] [001] \
 -duplicate 20 20 60 \
--select out cylinder Z 0.5*box 0.5*box 40 \
--remove-atom select \
-
 Al_alloy.lmp
 
 mv Al_alloy.lmp Al_alloy.lmpdat
