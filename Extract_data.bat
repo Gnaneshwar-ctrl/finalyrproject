3a8c03616562622e42211f9d36551ae6eef7f8dc
Gnaneshwar <gnaneshwarsampathirao13@gmail.com>
2022-09-19 18:54:29 +0530

    4update

---
+++ Extract_data.bat
@@ -0,0 +1,18 @@
+@echo off & py -x "%~f0" %*
+
+import re
+import csv
+
+with open('log.lammps', 'r') as f:
+
+    pattern = 'Step.*?\n(.*?)Loop'
+    string = f.read()
+
+    extracted_data = []
+    for total_data in re.findall(pattern, string, flags=re.DOTALL): 
+        for line in total_data.splitlines():
+            extracted_data.append(line.split())
+
+with open('Extracted_data.csv', 'w', newline='') as f:
+    write = csv.writer(f)
+    write.writerows(extracted_data)
